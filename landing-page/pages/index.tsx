80563d022a0b8a888023bb9ebaaf8788fc94dd69
Nkomha Lionel <nkomhalionel@gmail.com>
2025-09-12 13:01:02 +0100

    new commit

--- landing-page/pages/index.tsx
+++ landing-page/pages/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
 import HeroSection from '../components/HeroSection';
+import UserTypes from '../components/UserTypes';
 
 const Header = dynamic(() => import('../components/Header'), { ssr: false });
-const UserTypes = dynamic(() => import('../components/UserTypes'), { ssr: false });
 const Features = dynamic(() => import('../components/Features'), { ssr: false });
 const Pricing = dynamic(() => import('../components/Pricing'), { ssr: false });
 const DemoTrial = dynamic(() => import('../components/DemoTrial'), { ssr: false });
